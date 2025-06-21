c19cf6088b23c31c060ed7a026142b1cc6c3bc6a
spillbert32 <73699515+spillbert32@users.noreply.github.com>
2025-06-22 00:13:39 +0300

    Изменение структуры

--- script.js
+++ script.js
@@ -1,21 +1,70 @@
-function createTreeNode(person) {
+fetch('data.json')
+  .then(res => res.json())
+  .then(data => {
+    const peopleById = {};
+    data.forEach(person => {
+      peopleById[person.id] = person;
+      person.children = [];
+    });
+
+    // Связываем детей с родителями
+    data.forEach(person => {
+      if (person.parents) {
+        person.parents.forEach(parentId => {
+          const parent = peopleById[parentId];
+          if (parent) parent.children.push(person);
+        });
+      }
+    });
+
+    // Находим корневых предков (без родителей)
+    const roots = data.filter(person => !person.parents || person.parents.length === 0);
+
+    const treeContainer = document.getElementById('tree');
+    roots.forEach(root => {
+      const node = createTreeNode(root, peopleById);
+      treeContainer.appendChild(node);
+    });
+  });
+
+// Создание блока с человеком
+function createPersonBlock(person) {
+  const personDiv = document.createElement('div');
+  personDiv.className = 'person';
+
+  const fullName = document.createElement('div');
+  fullName.textContent = `${person.name} ${person.patronymic || ''}`.trim();
+  fullName.className = 'person-name';
+
+  const birthdate = document.createElement('div');
+  birthdate.textContent = person.birthdate || '';
+  birthdate.className = 'person-birthdate';
+
+  personDiv.appendChild(fullName);
+  if (birthdate.textContent) {
+    personDiv.appendChild(birthdate);
+  }
+
+  return personDiv;
+}
+
+// Рекурсивное создание дерева
+function createTreeNode(person, peopleById) {
   const container = document.createElement('div');
   container.className = 'person-container';
 
   const couple = document.createElement('div');
   couple.className = 'couple';
 
-  const personDiv = document.createElement('div');
-  personDiv.className = 'person';
-  personDiv.textContent = person.name;
-
+  const personDiv = createPersonBlock(person);
   couple.appendChild(personDiv);
 
-  if (person.spouse) {
-    const spouseDiv = document.createElement('div');
-    spouseDiv.className = 'person';
-    spouseDiv.textContent = person.spouse;
-    couple.appendChild(spouseDiv);
+  if (person.spouseId) {
+    const spouse = peopleById[person.spouseId];
+    if (spouse) {
+      const spouseDiv = createPersonBlock(spouse);
+      couple.appendChild(spouseDiv);
+    }
   }
 
   container.appendChild(couple);
@@ -25,7 +74,7 @@ function createTreeNode(person) {
     childrenDiv.className = 'children';
 
     person.children.forEach(child => {
-      const childNode = createTreeNode(child);
+      const childNode = createTreeNode(child, peopleById);
       childrenDiv.appendChild(childNode);
     });
 
@@ -34,11 +83,3 @@ function createTreeNode(person) {
 
   return container;
 }
-
-fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-    const treeContainer = document.getElementById('tree');
-    const tree = createTreeNode(data);
-    treeContainer.appendChild(tree);
-  });
