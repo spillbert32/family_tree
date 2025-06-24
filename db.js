6c382a1533d9218599a9dd7ffbf14b765f1ec6fa
spillbert32 <73699515+spillbert32@users.noreply.github.com>
2025-06-24 12:18:08 +0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,19 @@
+[
+  {
+    "id": 1,
+    "name": "Иван Иванович",
+    "gender": "male",
+    "birthYear": 1950,
+    "deathYear": 2020,
+    "parents": [],
+    "children": [4]
+  },
+  {
+    "id": 4,
+    "name": "Алексей Иванович",
+    "gender": "male",
+    "birthYear": 1980,
+    "parents": [1],
+    "children": []
+  }
+]
